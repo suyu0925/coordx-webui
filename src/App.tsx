21d58132190f01d70ddf406a44d717454fe63fd4
suyu <13936562@qq.com>
2024-12-30 23:47:58 +0800

    use responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -48,12 +48,12 @@ function App() {
       }))
     }
   }
-  const buttonClass = "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-  const inputClass = "w-64 p-2.5 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+  const buttonClass = "flex-shrink-0 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+  const inputClass = "flex-shrink-0 w-64 p-2.5 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
   return (
     <div className="flex flex-col p-4">
-      <div className="flex flex-row gap-2 items-baseline">
-        <div className="text-xl font-bold w-24">WGS 84</div>
+      <div className="flex flex-row flex-wrap gap-2 items-baseline">
+        <div className="flex-shrink-0 text-xl font-bold w-24">WGS 84</div>
         <input
           className={clsx(inputClass, state.source === 'WGS84' ? 'bg-blue-500 text-white' : 'bg-gray-50')}
           value={state.wgs84}
@@ -65,8 +65,8 @@ function App() {
           onClick={() => onConvert('WGS84')}>Convert</button>
       </div>
 
-      <div className="flex flex-row gap-2 items-baseline">
-        <div className="text-xl font-bold w-24">GCJ-02</div>
+      <div className="flex flex-row flex-wrap gap-2 items-baseline">
+        <div className="flex-shrink-0 text-xl font-bold w-24">GCJ-02</div>
         <input
           className={clsx(inputClass, state.source === 'GCJ02' ? 'bg-blue-500 text-white' : 'bg-gray-50')}
           value={state.gcj02}
@@ -84,8 +84,8 @@ function App() {
         </a>
       </div>
 
-      <div className="flex flex-row gap-2 items-baseline">
-        <div className="text-xl font-bold w-24">BD09</div>
+      <div className="flex flex-row flex-wrap gap-2 items-baseline">
+        <div className="flex-shrink-0 text-xl font-bold w-24">BD09</div>
         <input
           className={clsx(inputClass, state.source === 'BD09' ? 'bg-blue-500 text-white' : 'bg-gray-50')}
           value={state.bd09}
